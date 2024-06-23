902b2cc22c5d3eb50d1c0b5aca106b173c845ff5
cyanes25 <cyanes@adacapital.com.br>
2024-06-23 13:41:15 -0300

    Arrumei erro de concatenar verificando array null

--- src/controller/ValuesListingController/getConversionData.ts
+++ src/controller/ValuesListingController/getConversionData.ts
@@ -14,7 +14,7 @@ type SmartContractOps = {
     operationName: string;
     feedback: Feedback;
     tx: string;
- 
+
 
 }
 
@@ -25,107 +25,105 @@ export type ConversionData = {
     smartContractOps: SmartContractOps[];
     brlaAmount: string;
     usdAmount: string;
-    coin: string,
-    userDocument: string,
-    chain: string,
+    coin: string;
+    userDocument: string;
+    chain: string;
     usdToBrla: boolean;
     basePrice: string;
-    icon: IconProp
-    receiverAddress: string
+    icon: IconProp;
+    receiverAddress: string;
     tx: string;
     baseFee: string;
-   
+
 }
 
 export type ExpectedConversionData = {
 
     createdAt: string;
-    ops: SmartContractOps[];
+    ops: SmartContractOps[] | null;
     brlaAmount: string;
     usdAmount: string;
-    coin: string,
-    userDocument: string,
-    title: string,
+    coin: string;
+    userDocument: string;
+    title: string;
     usdToBrla: boolean;
     basePrice: string;
-    icon: IconProp
-    feedback: Feedback
-    operationName: string;
+    icon: IconProp;
+    feedback: Feedback | null;
+    operationName: string | null;
     isPayment: boolean;
-    tx: string;
-    isSwap: boolean,
+    tx: string | null;
+    isSwap: boolean;
     receiverAddress: string;
-
 }
 
 export async function getConversionData () {
-
+    
     try {
 
         const request = await http.get('/swap/history', {
             withCredentials: true
         });
-        
+
         const stringData = sessionStorage.getItem(USER_DATA);
-        
+
 
         if(stringData) {
 
             const userData : UserData = JSON.parse(stringData);
-    
+          
             const walletAddress = userData?.wallets.evm;
-            
-            const data = request.data.swapLogs.map((item: ConversionData) => ({
-    
-                createdAt: item.createdAt,
-    
-                ops: item.smartContractOps.map((op: SmartContractOps) => ({
-                    id: op.id,
-                    feedback: {
-                        ...op.feedback,
-                        createdAt: item.createdAt 
-                    }
-                    
-                })),
-    
-                tx: item.smartContractOps.reduce((acc: string, op: SmartContractOps) => {
-                    acc = op.tx;
-                    return acc;
-                },''),
-    
-                feedback: item.smartContractOps.reduce((acc: Feedback, op: SmartContractOps) => {
-                    acc = op.feedback;
-                    return acc;
-                }, {} as Feedback),
-    
-                usdAmount: (parseFloat(item.usdAmount) / TO_WEBSOCKET),
-                brlaAmount: parseFloat(item.brlaAmount) / TO_WEBSOCKET,
-                outputCoin: item.coin,
-                userDocument: item.userDocument,
-                title: item.chain,
-                operationName: 'SWAP',
-                basePrice: item.basePrice,
-                usdToBrla: item.usdToBrla,
-                icon: faArrowRightArrowLeft,
-                isPayment: item.receiverAddress !== walletAddress,
-                isSwap: true,
-                receiverAddress: item.receiverAddress
-               
-                
-                
-                
-            }));
 
+            const data = request.data.swapLogs.map((item: ConversionData) => {
+                const smartContractOps = item.smartContractOps || [];
+                
+                return {
+                    createdAt: item.createdAt,
+                 
+                    ops: smartContractOps.length > 0 ? smartContractOps.map((op: SmartContractOps) => ({
+                        id: op?.id,
+                        feedback: {
+                            ...op?.feedback,
+                            createdAt: item?.createdAt 
+                        }
+                    })) : null,
+
+                    tx: smartContractOps.length > 0 ? smartContractOps.reduce((acc: string, op: SmartContractOps) => {
+                        acc = op.tx;
+                        return acc;
+                    }, '') : null,
+                    feedback: smartContractOps.length > 0 ? smartContractOps.reduce((acc: Feedback, op: SmartContractOps) => {
+                        acc = op?.feedback;
+                        return acc;
+                    }, {} as Feedback) : null,
+                    usdAmount: (parseFloat(item.usdAmount) / TO_WEBSOCKET),
+                    brlaAmount: parseFloat(item.brlaAmount) / TO_WEBSOCKET,
+                    outputCoin: item.coin,
+                    userDocument: item.userDocument,
+                    title: item.chain,
+                    operationName: 'SWAP',
+                    basePrice: item.basePrice,
+                    usdToBrla: item.usdToBrla,
+                    icon: faArrowRightArrowLeft,
+                    isPayment: item.receiverAddress !== walletAddress,
+                    isSwap: true,
+                    receiverAddress: item.receiverAddress
+                
+                
+                
+                
+                }});
+                
 
             sessionStorage.setItem(SWAP_DATA, JSON.stringify(data));
-         
+
             return data;
 
         }
 
     } catch(e: any) {
-
-        throw new Error("Erro ao pegar dados de conversão: ", e.data?.message || e.message);
-      
+        
+        throw new Error("Erro ao pegar dados de conversão: " + (e.data?.message || e.message));
+    
     }
 }
